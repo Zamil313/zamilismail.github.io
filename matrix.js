e14569408b006e3609094bea40f191f5b1f2ebc1
Zamil313 <105770196+Zamil313@users.noreply.github.com>
2025-06-01 14:08:10 +0300

    matrix.js

--- matrix.js
+++ matrix.js
@@ -1,32 +1,30 @@
 const canvas = document.getElementById('matrix-canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 const letters = "01";
 const fontSize = 14;
 const columns = canvas.width / fontSize;
+const drops = Array(Math.floor(columns)).fill(1);
 
-const drops = new Array(Math.floor(columns)).fill(1);
-
-function drawMatrix() {
+function draw() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   ctx.fillStyle = "#0F0";
-  ctx.font = fontSize + "px monospace";
+  ctx.font = `${fontSize}px monospace`;
 
   for (let i = 0; i < drops.length; i++) {
-    const text = letters.charAt(Math.floor(Math.random() * letters.length));
+    const text = letters[Math.floor(Math.random() * letters.length)];
     ctx.fillText(text, i * fontSize, drops[i] * fontSize);
 
     if (drops[i] * fontSize > canvas.height || Math.random() > 0.975) {
       drops[i] = 0;
     }
-
     drops[i]++;
   }
 }
 
-setInterval(drawMatrix, 33);
+setInterval(draw, 33);
